86d0ab3d2d629f2dbc164da2f075571172eea2f9
abearxiong <xiongxiao1012@outlook.com>
2025-04-29 23:19:36 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,25 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-
+import sitemap from '@astrojs/sitemap';
+import pkgs from './package.json';
 import tailwindcss from '@tailwindcss/vite';
 
+const isDev = process.env.NODE_ENV === 'development';
 export default defineConfig({
   // ...
-  integrations: [mdx(), react()],
+  // site: 'https://kevisual.xiongxiao.me/root/astro',
+  base: isDev ? undefined : pkgs.basename,
+  integrations: [
+    mdx(),
+    react(), //
+    // sitemap(), // sitemap must be site has a domain
+  ],
 
   vite: {
     plugins: [tailwindcss()],
+    define: {
+      BASE_NAME: JSON.stringify(pkgs.basename),
+    },
   },
 });
